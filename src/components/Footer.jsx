a0de4afb96aec8070f3dfe8c0bbbad33923e84b7
Hamdan-075 <hamdanaslam446@gmail.com>
2026-06-13 22:46:43 +0500

    fix invalid import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import logo from '../assets/logo1.png';
 import { SiteContext } from '../context/SiteContext';
 import { getIcon } from '../utils/iconMap';
 import { FaAngleRight, FaArrowRight, FaEnvelope, FaCheckCircle, FaSpinner, FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
